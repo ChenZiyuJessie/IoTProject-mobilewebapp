c44b63e4e93fcb44c920cf81de643cdd9e42d905
ChenZiyuJessie <250714751@qq.com>
2018-04-23 01:12:22 +0700

    first commit

---
+++ src/pages/register/register.ts
@@ -0,0 +1,27 @@
+import { Component ,ViewChild} from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-register',
+  templateUrl: 'register.html',
+})
+export class RegisterPage {
+
+  @ViewChild('username') user;
+  @ViewChild('password') password;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad RegisterPage');
+  }
+
+  registerUser(){
+    console.log('Would sign in with', this.user.value, this.password.value)
+
+  }
+
+}
